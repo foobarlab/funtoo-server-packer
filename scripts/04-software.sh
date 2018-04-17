f56e29248474df591215ea96e18840516f50fb88
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-04-17 10:55:58 +0200

    disable saltstack for now

--- scripts/04-software.sh
+++ scripts/04-software.sh
@@ -8,10 +8,11 @@ fi
 # install ansible for automation
 sudo emerge -vt app-admin/ansible
 
-# install salt for automation
-sudo mkdir -p /etc/portage/package.use
-cat <<'DATA' | sudo tee -a /etc/portage/package.use/vbox-defaults
-app-admin/salt portage vim-syntax gnupg keyring timelib
-DATA
-sudo emerge -vt app-admin/salt
-sudo emerge -vt app-vim/salt-vim
+# TODO add saltstack
+## install salt for automation
+#sudo mkdir -p /etc/portage/package.use
+#cat <<'DATA' | sudo tee -a /etc/portage/package.use/vbox-defaults
+#app-admin/salt portage vim-syntax gnupg keyring timelib
+#DATA
+#sudo emerge -vt app-admin/salt
+#sudo emerge -vt app-vim/salt-vim
