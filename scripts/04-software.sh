c6d9c4d9806cd5b98939cd20a4a77b89d79ce8f4
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-04-17 10:53:26 +0200

    add ansible for automation

--- scripts/04-software.sh
+++ scripts/04-software.sh
@@ -5,15 +5,13 @@ if [ -z ${BUILD_RUN:-} ]; then
   exit 1
 fi
 
+# install ansible for automation
+sudo emerge -vt app-admin/ansible
+
 # install salt for automation
 sudo mkdir -p /etc/portage/package.use
 cat <<'DATA' | sudo tee -a /etc/portage/package.use/vbox-defaults
-app-admin/salt portage vim-syntax gnupg keyring openssl timelib
+app-admin/salt portage vim-syntax gnupg keyring timelib
 DATA
 sudo emerge -vt app-admin/salt
 sudo emerge -vt app-vim/salt-vim
-
-# FIXME configure saltstack
-
-# DEBUG: show eselect options
-sudo eselect
