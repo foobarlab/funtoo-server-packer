501ff09a7fbfcb2c8d36771b91ffe1632ca5a20f
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-05-08 15:58:39 +0200

    update notes, fix: not mounting /boot (already mounted)

--- scripts/05-spectre-fix.sh
+++ scripts/05-spectre-fix.sh
@@ -22,13 +22,12 @@ else
 	fi	
 fi
 
-# install spectre-metdown-checker for verification
+# install spectre-metdown-checker to verify applied fixes
 cd /usr/local/src
 sudo git clone https://github.com/speed47/spectre-meltdown-checker.git
-sudo mount /boot
 sudo /usr/local/src/spectre-meltdown-checker/spectre-meltdown-checker.sh
 
-# to fix spectre v2 in the linux kernel we need to recompile the kernel with gcc 7.3.1+
+# to fix spectre v2 we need to recompile the kernel with gcc 7.3.1+
 cd /usr/src/linux && sudo make distclean
 sudo genkernel --kernel-config=/usr/src/kernel.config --install initramfs all
 sudo eclean-kernel -n 1
@@ -39,5 +38,5 @@ source /etc/profile
 
 sudo boot-update
 
-# FIXME most likely needs a reboot to load the new kernel and to show up-to-date info
+# FIXME most likely needs a reboot to load the new kernel and to show up-to-date info, TODO move to vagrantfile internal script
 sudo /usr/local/src/spectre-meltdown-checker/spectre-meltdown-checker.sh
