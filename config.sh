620bce3d2ef168929b301a3f36d6f42077aaf567
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-07-04 15:23:32 +0200

    changed parent box naming scheme

--- config.sh
+++ config.sh
@@ -3,20 +3,19 @@
 . version.sh
 
 export BUILD_BOX_NAME="funtoo-server"
+export BUILD_BOX_USERNAME="foobarlab"
 
 export BUILD_PARENT_BOX_NAME="funtoo-core"
-export BUILD_PARENT_BOX_VAGRANTCLOUD_NAME="foobarlab/funtoo-core"
+export BUILD_PARENT_BOX_VAGRANTCLOUD_NAME="$BUILD_BOX_USERNAME/$BUILD_PARENT_BOX_NAME"
 
 export BUILD_GUEST_TYPE="Gentoo_64"
 export BUILD_GUEST_CPUS="4"
 export BUILD_GUEST_MEMORY="4096"
 
-export BUILD_BOX_PROVIDER="virtualbox"
-export BUILD_BOX_USERNAME="foobarlab"
-
 export BUILD_OUTPUT_FILE="$BUILD_BOX_NAME-$BUILD_BOX_VERSION.box"
 export BUILD_OUTPUT_FILE_TEMP="$BUILD_BOX_NAME.tmp.box"
 
+export BUILD_BOX_PROVIDER="virtualbox"
 export BUILD_BOX_RELEASE_NOTES="Funtoo 1.2 preview, GCC 7.3.1, Debian Kernel, includes Ansible"
 export BUILD_BOX_DESCRIPTION="$BUILD_BOX_RELEASE_NOTES<br>$BUILD_BOX_NAME build @$(date --iso-8601=seconds)"
 
