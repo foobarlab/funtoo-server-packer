77ee750e236c55ebcbb423b09ba12a9b7b574084
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-07-11 09:50:29 +0200

    testing: disable ssl for rsyslog to avoid compile failure

--- scripts/01-prepare.sh
+++ scripts/01-prepare.sh
@@ -17,6 +17,7 @@ fi
 sudo mkdir -p /etc/portage/package.use
 cat <<'DATA' | sudo tee -a /etc/portage/package.use/vbox-defaults
 app-misc/mc -edit
+app-admin/rsyslog -ssl
 DATA
 
 sudo epro flavor server
