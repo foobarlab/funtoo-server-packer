c6ec5ad6e3248b5e421900f8c060b198030b8877
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-08-01 14:17:47 +0200

    prevent premature termination (already mounted gives an exit code 32)

--- scripts/04-spectre.sh
+++ scripts/04-spectre.sh
@@ -40,6 +40,6 @@ cd /usr/local/src
 sudo git clone https://github.com/speed47/spectre-meltdown-checker.git
 
 # report current Spectre/Meltdown status
-# TODO check status results: probably needs a reboot to load the new kernel and to show up-to-date info
-sudo mount /boot
+# TODO check status results: might need a reboot to load the new kernel and show up-to-date info
+sudo mount /boot || true
 sudo /usr/local/src/spectre-meltdown-checker/spectre-meltdown-checker.sh -v 2>/dev/null || true
