d1b729fab4d148a988cb65dc39cc09c4b00192cf
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-08-02 15:25:06 +0200

    put all services in single file

--- scripts/05-services.sh
+++ scripts/05-services.sh
@@ -5,6 +5,13 @@ if [ -z ${BUILD_RUN:-} ]; then
   exit 1
 fi
 
+# logging facility
+sudo emerge -vt app-admin/rsyslog
+sudo rc-update add rsyslog default
+
 # cron service
 sudo emerge -vt sys-process/cronie
 sudo rc-update add cronie default
+
+# mail transport agent
+sudo emerge -vt mail-mta/postfix
--- scripts/07-email.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -uex
-
-if [ -z ${BUILD_RUN:-} ]; then
-  echo "This script can not be run directly! Aborting."
-  exit 1
-fi
-
-# mail transport agent
-sudo emerge -vt mail-mta/postfix
